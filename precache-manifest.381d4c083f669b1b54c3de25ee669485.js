fad1ba8d1d16549226e9865943ffcdc76323b0ee
atefgad <atef0777@gmail.com>
2019-03-06 12:12:56 +0200

    Updates

---
+++ precache-manifest.381d4c083f669b1b54c3de25ee669485.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e727764bb7e62f654e01331fe02583b",
+    "url": "/portfolioz-react-template/static/media/profile.2e727764.png"
+  },
+  {
+    "revision": "8126c865839c473ea122",
+    "url": "/portfolioz-react-template/static/js/runtime~main.8126c865.js"
+  },
+  {
+    "revision": "f78fe9f259f9882555b2",
+    "url": "/portfolioz-react-template/static/js/main.f78fe9f2.chunk.js"
+  },
+  {
+    "revision": "5918465d849890407b23",
+    "url": "/portfolioz-react-template/static/js/2.5918465d.chunk.js"
+  },
+  {
+    "revision": "f78fe9f259f9882555b2",
+    "url": "/portfolioz-react-template/static/css/main.d444fc50.chunk.css"
+  },
+  {
+    "revision": "5918465d849890407b23",
+    "url": "/portfolioz-react-template/static/css/2.1b66ad2e.chunk.css"
+  },
+  {
+    "revision": "5c41911d59709ba177aa056c951ed15b",
+    "url": "/portfolioz-react-template/index.html"
+  }
+];
